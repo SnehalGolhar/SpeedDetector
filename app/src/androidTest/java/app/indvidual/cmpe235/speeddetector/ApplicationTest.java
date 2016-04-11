b5bb9db81a84313bf6f3fd94ceebc3f4eb3141c0
snehalgolhar <snehal.golhar89@gmail.com>
2016-04-11 13:29:29 -0700

    Initial commit

---
+++ app/src/androidTest/java/app/indvidual/cmpe235/speeddetector/ApplicationTest.java
@@ -0,0 +1,13 @@
+package app.indvidual.cmpe235.speeddetector;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
